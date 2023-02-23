7cfff03b03ca09fca3a5f403932cb5fc0afdd04f
Yesica Perlaza <perlazayesik@gmail.com>
2023-02-22 20:58:49 -0500

    barra de navegacion terminada

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict"
+    const d = document;
+
+    //Barra de navegacion
+
+    const contNavItems = d.querySelector(".nav-cont-items");
+    const btnMenu = d.querySelector(".btn-menu");
+    const btnClose = d.querySelector(".btn-close");
+
+    d.addEventListener("click",(e)=>{
+        if(e.target.matches(".btn-menu")){
+            contNavItems.classList.add("nav-cont-items-show");
+            btnMenu.classList.add("btn-show");
+            btnClose.classList.remove("btn-show");
+        }
+        if(e.target.matches(".btn-close")){
+            contNavItems.classList.remove("nav-cont-items-show");
+            btnMenu.classList.remove("btn-show");
+            btnClose.classList.add("btn-show");
+        }
+    })
+})();
