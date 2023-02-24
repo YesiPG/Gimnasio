2e15baa1979aae0d111b685beeff3754b50fcc9a
Yesica Perlaza <perlazayesik@gmail.com>
2023-02-24 15:47:58 -0500

    la section contacto terminada

--- js/main.js
+++ js/main.js
@@ -25,9 +25,34 @@
     
     // Validacion de formulario
     const btnEviar = d.querySelector(".btn-enviar");
+    const btnReset = d.querySelector(".btn-reset");
+    const nombreForm = d.getElementById("nombreForm").value;
+    const correoForm = d.getElementById("correoForm").value;
+    const pNombre = d.querySelector(".p-nombre");
+    const pCorreo = d.querySelector(".p-correo");
 
     btnEviar.addEventListener("click",(e)=>{
-        // e.preventDefault();
+        e.preventDefault();
 
+        let regexNombre = /^[a-zA-ZÀ-ÿ\s]+$/;
+        let regexCorreo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$/;
+        
+        if(!regexNombre.test(nombreForm)){
+            pNombre.innerHTML = "El dato que ingresaste no es un nombre";
+        }
+        console.log(regexNombre.test(nombreForm));
+
+        if(!regexCorreo.test(correoForm)){
+            pCorreo.innerHTML = "No se ingreso correctamente el correo";
+        }
+        console.log(regexCorreo.test(correoForm));
+
+        
+    });
+
+    btnReset.addEventListener("click",(e)=>{
+        pNombre.innerHTML = "";
+        pCorreo.innerHTML = "";   
     })
+
 })();
